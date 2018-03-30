f862b6017cca69e3599f547d5f15a6272f29b1c3
Andreas Longva <andreas.b.longva@gmail.com>
2018-03-30 13:14:24 +0200

    Add untested Shader/ShaderProgram classes

---
+++ src/shader.cpp
@@ -0,0 +1,67 @@
+#include "shader.hpp"
+
+#include <stdexcept>
+
+namespace merely3d
+{
+    static GLuint convert_to_gl_shader_type(ShaderType type)
+    {
+        switch (type)
+        {
+            case ShaderType::Fragment: return GL_FRAGMENT_SHADER;
+            case ShaderType::Vertex: return GL_VERTEX_SHADER;
+        }
+    }
+
+    Shader Shader::compile(ShaderType type, const std::string & source)
+    {
+        const auto gl_fragment_type = convert_to_gl_shader_type(type);
+        const auto shader_id = glCreateShader(gl_fragment_type);
+
+        const auto source_c_str = source.c_str();
+
+        glShaderSource(shader_id, 1, &source_c_str, NULL);
+        glCompileShader(shader_id);
+
+        int  success;
+        glGetShaderiv(shader_id, GL_COMPILE_STATUS, &success);
+
+        if (!success)
+        {
+            char info_log[512];
+            std::string error_msg(info_log);
+            throw std::runtime_error("Shader compilation error: " + error_msg);
+        }
+
+        Shader shader;
+        shader._id = shader_id;
+        return shader;
+    }
+
+    ShaderProgram ShaderProgram::create()
+    {
+        ShaderProgram program;
+        program._id = glCreateProgram();
+        return program;
+    }
+
+    void ShaderProgram::attach(const Shader & shader)
+    {
+        glAttachShader(_id, shader._id);
+    }
+
+    void ShaderProgram::link()
+    {
+        glLinkProgram(_id);
+
+        int success;
+        glGetProgramiv(_id, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            char info_log[512];
+            glGetProgramInfoLog(_id, 512, NULL, info_log);
+            std::string error_msg(info_log);
+            throw std::runtime_error("Program link error: " + error_msg);
+        }
+    }
+}
---
+++ src/shader.hpp
@@ -0,0 +1,53 @@
+#include <string>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+namespace merely3d
+{
+    enum class ShaderType
+    {
+        Vertex,
+        Fragment
+    };
+
+    // TODO: Cleanup shaders when they are not needed anymore, somehow?
+    class Shader
+    {
+    public:
+
+    private:
+        Shader() : _id(0) {}
+
+        /// Compiles the shader, provided as source code in a string.
+        ///
+        /// Note that the correct context *must* have been set before compiling the shader.
+        static Shader compile(ShaderType type, const std::string & source);
+
+    private:
+        GLuint _id;
+
+        friend class ShaderProgram;
+    };
+
+    // TODO: Cleanup programs when they are not needed anymore?
+    class ShaderProgram
+    {
+    public:
+
+        /// Creates a new shader program. The context *must* have correctly been set beforehand.
+        static ShaderProgram create();
+
+        /// Attaches the given shader to this program. The context *must* have correctly been set beforehand.
+        void attach(const Shader & shader);
+
+        /// Links the current program. The context *must* have correctly been set beforehand.
+        void link();
+
+        /// Makes GL use the current program. The context *must* have correctly been set beforehand.
+        void use();
+
+    private:
+        GLuint _id;
+    };
+}
