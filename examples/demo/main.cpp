a5595b8be744906a7add2cb96f25f88182f59923
Andreas Longva <longva@cs.rwth-aachen.de>
2018-04-11 10:32:16 +0200

    Implement App for easier initialization

--- examples/demo/main.cpp
+++ examples/demo/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <algorithm>
 
+#include <merely3d/app.hpp>
 #include <merely3d/window.hpp>
 #include <merely3d/camera_controller.hpp>
 
@@ -28,45 +29,44 @@ using Eigen::Vector3f;
 
 int main(void)
 {
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+    // Constructing the app first is essential: it makes sure that
+    // GLFW is set up properly. Note that as an alternative, you can call
+    // glfw init/terminate yourself directly, but you must be careful that
+    // any windows are destroyed before calling terminate(). App automatically
+    // takes care of this as long as it outlives any windows.
+    merely3d::App app;
 
-    // Scope is necessary because Window calls GL destructors in constructor
+    auto window = WindowBuilder()
+                   .dimensions(1024, 768)
+                   .title("Hello merely3d!")
+                   .multisampling(8)
+                   .build();
+
+    window.camera().look_in(Vector3f(1.0, 0.0, -1), Vector3f(0.0, 0.0, 1.0));
+    window.camera().set_position(Vector3f(-1.0, 0.0, 3.0));
+
+    window.add_event_handler(std::shared_ptr<EventHandler>(new CameraController));
+
+    while (!window.should_close())
     {
-        auto window = WindowBuilder()
-                    .dimensions(1024, 768)
-                    .title("Hello merely3d!")
-                    .multisampling(8)
-                    .build();
-
-        window.camera().look_in(Vector3f(1.0, 0.0, -1), Vector3f(0.0, 0.0, 1.0));
-        window.camera().set_position(Vector3f(-1.0, 0.0, 3.0));
-
-        window.add_event_handler(std::shared_ptr<EventHandler>(new CameraController));
-
-        while (!window.should_close())
+        window.render_frame([] (Frame & frame)
         {
-            window.render_frame([] (Frame & frame)
-            {
-                const auto rect_material = Material().with_color(Color(0.5, 0.3, 0.3));
-                Orientation orientation = Orientation::Identity();
-                orientation = Eigen::AngleAxisf(0.78, Eigen::Vector3f(1.0, 0.0, 0.0));
-                frame.draw_rectangle(Vector2(0.5, 0.5),
-                                     Position(1.0, 0.0, 0.05),
-                                     orientation,
-                                     rect_material);
-                frame.draw_box(Vector3(1.0, 1.0, 1.0), Position(4.0, 0.0, 0.1));
+            const auto rect_material = Material().with_color(Color(0.5, 0.3, 0.3));
+            Orientation orientation = Orientation::Identity();
+            orientation = Eigen::AngleAxisf(0.78, Eigen::Vector3f(1.0, 0.0, 0.0));
+            frame.draw_rectangle(Vector2(0.5, 0.5),
+                                 Position(1.0, 0.0, 0.05),
+                                 orientation,
+                                 rect_material);
+            frame.draw_box(Vector3(1.0, 1.0, 1.0), Position(4.0, 0.0, 0.1));
 
-                frame.draw_box(Vector3(0.3, 5.0, 3.0), Position(0.0, 0.0, 0.0));
-                frame.draw_box(Vector3(0.2, 1.0, 1.0),
-                               Position(0.0, 0.0, 5.0),
-                               Orientation(Eigen::AngleAxisf(0.5, Vector3(1.0, 1.0, 1.0))),
-                               Material().with_color(Color(1.0, 0.0, 0.0)));
-            });
-        }
+            frame.draw_box(Vector3(0.3, 5.0, 3.0), Position(0.0, 0.0, 0.0));
+            frame.draw_box(Vector3(0.2, 1.0, 1.0),
+                           Position(0.0, 0.0, 5.0),
+                           Orientation(Eigen::AngleAxisf(0.5, Vector3(1.0, 1.0, 1.0))),
+                           Material().with_color(Color(1.0, 0.0, 0.0)));
+        });
     }
 
-    glfwTerminate();
     return 0;
 }
